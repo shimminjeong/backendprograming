83944cbfd51bc0c24cbfca250a3dd1651b170643
simminjeonghome <pooh5045@naver.com>
2023-08-18 13:38:36 +0900

    mybatis select

--- mybatis/src/mybatis/BoardDAO.java
+++ mybatis/src/mybatis/BoardDAO.java
@@ -18,19 +18,22 @@ public class BoardDAO {
 
 	public void work() {
 //		insertNewPost();
-		selectAllPost();
+//		selectAllPost();
+		selectbyName();
+//		selectbyName2();
+//		selectbyNo();
 	}
 
 	public void selectAllPost() {
 		List<BoardVO> boardList = sqlSession.selectList("mybatis.BoardDAO.selectAll");
 		for (BoardVO board : boardList) {
-            System.out.println("Board NO: " + board.getNo());
-            System.out.println("Board NO: " + board.getWriter());
-            System.out.println("Title: " + board.getTitle());
-            System.out.println("Content: " + board.getContent());
-            // ... 다른 멤버 변수 출력
-            System.out.println("-------------------------------------");
-        }
+			System.out.println("Board NO: " + board.getNo());
+			System.out.println("Board NO: " + board.getWriter());
+			System.out.println("Title: " + board.getTitle());
+			System.out.println("Content: " + board.getContent());
+			// ... 다른 멤버 변수 출력
+			System.out.println("-------------------------------------");
+		}
 	}
 
 	public void insertNewPost() {
@@ -42,6 +45,47 @@ public class BoardDAO {
 		boardVO.setContent("내용이요");
 		sqlSession.insert("mybatis.BoardDAO.insertNewPost2", boardVO);
 		sqlSession.commit();
+	}
+
+	public void selectbyName() {
+		BoardVO boardVO = new BoardVO();
+		boardVO.setWriter("김길동");
+		List<BoardVO> boardList = sqlSession.selectList("mybatis.BoardDAO.selectbyName", boardVO);
+		for (BoardVO board : boardList) {
+			System.out.println("Board NO: " + board.getNo());
+			System.out.println("Board NO: " + board.getWriter());
+			System.out.println("Title: " + board.getTitle());
+			System.out.println("Content: " + board.getContent());
+			System.out.println("regdate: " + board.getRegDate());
+			// ... 다른 멤버 변수 출력
+			System.out.println("-------------------------------------");
+		}
+	}
+
+	public void selectbyName2() {
+		String name = "김길동";
+		List<BoardVO> boardList = sqlSession.selectList("mybatis.BoardDAO.selectbyName2", name);
+		for (BoardVO board : boardList) {
+			System.out.println("Board NO: " + board.getNo());
+			System.out.println("Board NO: " + board.getWriter());
+			System.out.println("Title: " + board.getTitle());
+			System.out.println("Content: " + board.getContent());
+			// ... 다른 멤버 변수 출력
+			System.out.println("-------------------------------------");
+		}
+	}
+
+	public void selectbyNo() {
+		int no = 10;
+		List<BoardVO> boardList = sqlSession.selectList("mybatis.BoardDAO.selectbyNo", no);
+		for (BoardVO board : boardList) {
+			System.out.println("Board NO: " + board.getNo());
+			System.out.println("Writer: " + board.getWriter());
+			System.out.println("Title: " + board.getTitle());
+			System.out.println("Content: " + board.getContent());
+			// ... 다른 멤버 변수 출력
+			System.out.println("-------------------------------------");
+		}
 	}
 
 }
