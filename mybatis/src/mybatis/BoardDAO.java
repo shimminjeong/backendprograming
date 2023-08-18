1678947c84b9c68acc53d7968daba14be8fb5e32
simminjeonghome <pooh5045@naver.com>
2023-08-18 11:13:46 +0900

    select문 추가

--- mybatis/src/mybatis/BoardDAO.java
+++ mybatis/src/mybatis/BoardDAO.java
@@ -1,5 +1,7 @@
 package mybatis;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 
 public class BoardDAO {
@@ -15,17 +17,30 @@ public class BoardDAO {
 	}
 
 	public void work() {
-		insertNewPost();
+//		insertNewPost();
+		selectAllPost();
+	}
+
+	public void selectAllPost() {
+		List<BoardVO> boardList = sqlSession.selectList("mybatis.BoardDAO.selectAll");
+		for (BoardVO board : boardList) {
+            System.out.println("Board NO: " + board.getNo());
+            System.out.println("Board NO: " + board.getWriter());
+            System.out.println("Title: " + board.getTitle());
+            System.out.println("Content: " + board.getContent());
+            // ... 다른 멤버 변수 출력
+            System.out.println("-------------------------------------");
+        }
 	}
 
 	public void insertNewPost() {
 //		sqlSession.insert("mybatis.BoardDAO.insertNewPost");
 //		sqlSession.commit();
-		BoardVO boardVO=new BoardVO();
-		boardVO.setTitle("두번째 제목");
+		BoardVO boardVO = new BoardVO();
+		boardVO.setTitle("세번째 제목");
 		boardVO.setWriter("김길동");
 		boardVO.setContent("내용이요");
-		sqlSession.insert("mybatis.BoardDAO.insertNewPost2",boardVO);
+		sqlSession.insert("mybatis.BoardDAO.insertNewPost2", boardVO);
 		sqlSession.commit();
 	}
 
