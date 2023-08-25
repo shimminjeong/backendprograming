c4859fabb80d34754d13d3bc25dae6b405763357
simminjeonghome <pooh5045@naver.com>
2023-08-25 10:15:56 +0900

    login session (HttpSession, @SessionAttributes)

--- springboard/src/main/java/kr/ac/kopo/member/controller/MemberController.java
+++ springboard/src/main/java/kr/ac/kopo/member/controller/MemberController.java
@@ -1,5 +1,9 @@
 package kr.ac.kopo.member.controller;
 
+import java.lang.ProcessBuilder.Redirect;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +13,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import kr.ac.kopo.member.service.MemberService;
 import kr.ac.kopo.member.vo.LoginVO;
 import kr.ac.kopo.member.vo.MemberVO;
 
+@SessionAttributes("currentUser")
 @Controller
 public class MemberController {
 
@@ -28,7 +35,8 @@ public class MemberController {
 	}
 
 	@PostMapping("/login")
-	public String loginProcess(@Valid LoginVO loginVO, Errors errors, Model model) {
+	public String loginProcess(@Valid LoginVO loginVO, Errors errors, Model model, HttpServletRequest request,
+			HttpSession session) {
 
 		if (errors.hasErrors()) { // error가 존재
 			return "member/loginForm"; // 다시 로그인하는 페이지로 이동
@@ -40,23 +48,44 @@ public class MemberController {
 				model.addAttribute("msg", "id, password 다시 확인하세요");
 				return "member/loginForm"; // 다시 로그인하는 페이지로 이동
 			} else {
-				System.out.println(memberVO);
 				System.out.println("login이 완료되었습니다.");
+				// 세션 등록 jsp ${currentUser}
+//				1) session.setAttribute("currentUser", memberVO);
+//				2) @SessionAttributes("currentUser")를 class명 위에 설정
+				model.addAttribute("currentUser", memberVO);
 				return "redirect:/board";
 				// http://localhost:8080/springboard/board
 			}
 		}
 	}
-	
+
+//	@GetMapping("/logout")
+//	public String logout(HttpSession session) {
+////		session.setAttribute("currentUser", memberVO);
+////		session.removeAttribute("currentUser");
+//		session.invalidate(); // setAttribute와 pair (@SessionAttributes("currentUser")에서는 작동하지 않음
+////		"index" WEB-INF/jsp/index.jsp (x)
+////		webapp/index.jsp
+//		// http://localhost:8080/springboard/
+//		return "redirect:/";
+//	}
+
+	@GetMapping("/logout")
+	public String logout2(SessionStatus sessionStatus) {
+//		session.invalidate(); //setAttribute와 pair에서는 작동하지 않음
+		sessionStatus.setComplete(); // @SessionAttributes에 등록되어 있는 변수들
+		return "redirect:/";
+	}
+
 	@GetMapping("/join")
 	public String joinForm(Model model) {
-		MemberVO memberVO=new MemberVO();
+		MemberVO memberVO = new MemberVO();
 		model.addAttribute(memberVO);
 		return "member/joinForm";
 	}
-	
+
 	@PostMapping("/join")
-	public String joinProcess(@Valid MemberVO memberVO,Errors errors,Model model)  {
+	public String joinProcess(@Valid MemberVO memberVO, Errors errors, Model model) {
 		if (errors.hasErrors()) {
 			System.out.println("null있음");
 			model.addAttribute("msg", "null이 있어요");
@@ -67,6 +96,5 @@ public class MemberController {
 			return "redirect:/board";
 		}
 	}
-	
 
 }
--- springboard/src/main/webapp/index.jsp
+++ springboard/src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+   pageEncoding="UTF-8"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,10 +8,21 @@
 <title>Insert title here</title>
 </head>
 <body>
-welcome
-<a href="${pageContext.request.contextPath}/board">게시판</a>
-<a href="${pageContext.request.contextPath}/board/new">새글쓰기</a>
-<a href="${pageContext.request.contextPath}/login">로그인</a>
-<a href="${pageContext.request.contextPath}/join">회원가입</a>
+   환영합니다. 첫 페이지에요.
+   <br> 1. 만약 currentUser 있나? 2. ${currentUser.name}님 안녕하세요. 3. 로그아웃
+   링크 생성, 로그인 링크 삭제
+
+   <a href="${pageContext.request.contextPath}/board">게시판</a>
+   <a href="${pageContext.request.contextPath}/board/new">글쓰기</a>
+   <c:if test="${empty currentUser}">
+      <a href="${pageContext.request.contextPath}/login">로그인</a>
+      <br>
+   </c:if>
+   <c:if test="${not empty currentUser}">
+      ${currentUser.name}님 안녕하세요.<br>
+      <a href="${pageContext.request.contextPath}/logout">로그아웃</a>
+      <br>
+   </c:if>
+
 </body>
 </html>
